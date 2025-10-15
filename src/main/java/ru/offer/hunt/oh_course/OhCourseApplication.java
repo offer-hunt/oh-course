2045a8d5fac5f496f7b8a1662c765d3d026a222a
termoler <misaib@yandex.ru>
2025-10-15 16:29:12 +0700

    add Java project with Gradle Kotlin DSL and basic CI

---
+++ src/main/java/ru/offer/hunt/oh_course/OhCourseApplication.java
@@ -0,0 +1,11 @@
+package ru.offer.hunt.oh_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhCourseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OhCourseApplication.class, args);
+	}
+}
